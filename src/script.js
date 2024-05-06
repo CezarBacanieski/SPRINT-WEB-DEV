c96099d3cba3a37b7a53e54256b216480bd8d157
Cezar Bacanieski <bacanieskidev@gmail.com>
2024-05-06 14:21:07 -0300

    function para aparecer as tarefas na tela e arrumando list html

--- src/script.js
+++ src/script.js
@@ -14,8 +14,20 @@ function newTask() {
     //guardar no local storage
     let values = JSON.parse(localStorage.getItem("localStorageKey") || "[]");
     values.push({
-        name: input.value
-    })
-    localStorage.setItem(localStorageKey, JSON.stringify(values))
+      name: input.value,
+    });
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+    show();
   }
 }
+
+function show() {
+  let values = JSON.parse(localStorage.getItem("localStorageKey") || "[]");
+  let list = document.getElementById("to-do-list");
+  list.innerHTML = "";
+  for (let i = 0; i < values.length; i++) {
+    list.innerHTML = `<li>${values[i]["name"]}</li>`;
+  }
+}
+
+show()
