3ae930ccafd8ddb00e5a868f4211fb61c42f8c25
Cezar Bacanieski <bacanieskidev@gmail.com>
2024-05-06 14:52:15 -0300

    apagando texto apos enviar, tbm arrumando botao do ok e verificando se há valor igual para aalertar

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
 const localStorageKey = "to-do-list"; // Definindo o nome da chave do localStorage
 
+function validateIfExistNewTask() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let inputValue = document.getElementById("input-nova-tarefa");
+  let exists = values.find((x) => x.name == inputValue.value);
+  return !exists ? false : true;
+}
+
 function newTask() {
   let input = document.getElementById("input-nova-tarefa");
+  input.style.border = "";
 
   // Validação
   if (!input.value) {
+    input.style.border = "1px solid red";
     alert("Você precisa escrever algo para adicionar a sua lista!");
-  } else if(validateIfExistNewTask()) {
-
-  }
-  else {
+  } else if (validateIfExistNewTask()) {
+    alert("Já existe uma terefa com essa descrição!");
+  } else {
     // Guardar no localStorage
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     values.push({
@@ -18,6 +26,7 @@ function newTask() {
     localStorage.setItem(localStorageKey, JSON.stringify(values));
     show();
   }
+  input.value = "";
 }
 
 function show() {
@@ -25,7 +34,9 @@ function show() {
   let list = document.getElementById("to-do-list");
   list.innerHTML = "";
   for (let i = 0; i < values.length; i++) {
-    list.innerHTML += `<li>${values[i]["name"]}<button id="btn-ok" onclick="removeItem('${values[i]["name"]}')">ok</button></li>`;
+    list.innerHTML += `<li>${values[i]["name"]}<button id="btn-ok" onclick="removeItem('${values[i]["name"]}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+  </svg></button></li>`;
     // Adicionando cada item à lista
   }
 }
@@ -35,7 +46,7 @@ function removeItem(data) {
   let index = values.findIndex((x) => x.name == data);
   values.splice(index, 1);
   localStorage.setItem(localStorageKey, JSON.stringify(values));
-  show()
+  show();
 }
 
 show(); // Chamando a função show() para exibir os itens já salvos ao carregar a página
