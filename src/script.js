f990dc59377ffc0c270576502ebcfa1b61e1e787
Cezar Bacanieski <bacanieskidev@gmail.com>
2024-05-06 14:08:11 -0300

    validacao de mensagem feito em js

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function newTask() {
+    let input = document.getElementById("input-nova-tarefa")
+
+    //validacao
+    if(!input.value) {
+        alert("Você precisa escrever algo para adicionar a sua lisat!")
+    // } else if() {
+
+    // }
+    else {
+        //guardar no local storage
+    }
+}
