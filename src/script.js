fc039b56157d13d54f47a4130b5b85898dc3c75e
Cezar Bacanieski <bacanieskidev@gmail.com>
2024-05-06 14:38:52 -0300

    function para remover tarefas da lista

--- src/script.js
+++ src/script.js
@@ -6,7 +6,10 @@ function newTask() {
   // Validação
   if (!input.value) {
     alert("Você precisa escrever algo para adicionar a sua lista!");
-  } else {
+  } else if(validateIfExistNewTask()) {
+
+  }
+  else {
     // Guardar no localStorage
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     values.push({
@@ -22,12 +25,17 @@ function show() {
   let list = document.getElementById("to-do-list");
   list.innerHTML = "";
   for (let i = 0; i < values.length; i++) {
-    list.innerHTML += `<li>${values[i]["name"]}<button id="btn-ok" onclick="removeItem("${values[i]["name"]}")">ok</button></li>`; // Adicionando cada item à lista
+    list.innerHTML += `<li>${values[i]["name"]}<button id="btn-ok" onclick="removeItem('${values[i]["name"]}')">ok</button></li>`;
+    // Adicionando cada item à lista
   }
 }
 
 function removeItem(data) {
-    console.log(data)
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let index = values.findIndex((x) => x.name == data);
+  values.splice(index, 1);
+  localStorage.setItem(localStorageKey, JSON.stringify(values));
+  show()
 }
 
 show(); // Chamando a função show() para exibir os itens já salvos ao carregar a página
