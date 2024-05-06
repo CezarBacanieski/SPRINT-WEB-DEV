e147b2d4d5fe54d8668c3ac90636735a23ee3869
Cezar Bacanieski <bacanieskidev@gmail.com>
2024-05-06 14:30:47 -0300

    function para mostrar tarefas na tela completa

--- src/script.js
+++ src/script.js
@@ -1,18 +1,14 @@
-const localStorageKey = "to-do-list";
+const localStorageKey = "to-do-list"; // Definindo o nome da chave do localStorage
 
 function newTask() {
   let input = document.getElementById("input-nova-tarefa");
 
-  //validacao
+  // Validação
   if (!input.value) {
     alert("Você precisa escrever algo para adicionar a sua lista!");
-  }
-  //  else if() {
-
-  //  }
-  else {
-    //guardar no local storage
-    let values = JSON.parse(localStorage.getItem("localStorageKey") || "[]");
+  } else {
+    // Guardar no localStorage
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
     values.push({
       name: input.value,
     });
@@ -22,12 +18,16 @@ function newTask() {
 }
 
 function show() {
-  let values = JSON.parse(localStorage.getItem("localStorageKey") || "[]");
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
   let list = document.getElementById("to-do-list");
   list.innerHTML = "";
   for (let i = 0; i < values.length; i++) {
-    list.innerHTML = `<li>${values[i]["name"]}</li>`;
+    list.innerHTML += `<li>${values[i]["name"]}<button id="btn-ok" onclick="removeItem("${values[i]["name"]}")">ok</button></li>`; // Adicionando cada item à lista
   }
 }
 
-show()
+function removeItem(data) {
+    console.log(data)
+}
+
+show(); // Chamando a função show() para exibir os itens já salvos ao carregar a página
